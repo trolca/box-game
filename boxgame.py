750a9f9716f43c9c37938dabc66f7332869171c2
trolca <szgiemza@gmail.com>
2023-04-21 20:06:07 +0200

    added the outline of the menu

--- boxgame.py
+++ boxgame.py
@@ -1,3 +1,22 @@
 import os
 import msvcrt as ms
+import time
 
+height = 17
+width = 12
+
+backround = "▒▒"
+boxtext = "▣"
+
+player = [0, 5]
+boxes = [[2, 30], [5, 30]]
+
+def drawScreen(width, height):
+    for y in range(0, height):
+        for x in range(0, width):
+            if y == 0 or y == height-1 or x == 0 or x == width-1: print(backround, end="")
+            else: print("  ", end="")
+        
+        print()
+
+drawScreen(width, height)
